58ace323a20b964061ec5e2f18c625266f467bc7
Malekaadutse <kamzazz@yahoo.com>
2025-08-09 21:30:52 +0200

    larosadeploy3
    kamo larosadeploy3

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   typescript: {
-    // Uncomment the line below if you want to ignore TypeScript build errors
     // ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
